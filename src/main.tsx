d9bc1603086df81569781ea699a826ec0eaaabb7
Hans Yulian <hansyulian@windowslive.com>
2024-09-11 09:46:15 +0700

    mantine setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { theme } from "./config/theme.tsx";
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
+  </StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      postcss: {
+        plugins: {
+          "postcss-preset-mantine": {},
+          "postcss-simple-vars": {
+            variables: {
+              "mantine-breakpoint-xs": "36em",
+              "mantine-breakpoint-sm": "48em",
+              "mantine-breakpoint-md": "62em",
+              "mantine-breakpoint-lg": "75em",
+              "mantine-breakpoint-xl": "88em",
+            },
+          },
+        },
+      },
+    },
+  },
+});
